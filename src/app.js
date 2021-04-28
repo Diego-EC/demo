da4a10da7a84b9ea650afc683c50eabb2637bdb7
Diego-EC <diegoezquerro@gmail.com>
2021-04-28 17:24:45 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+
+// 2º capturar evento click del botón
+document.querySelector("#drawCards").addEventListener("click", e => {
+  e.preventDefault();
+  console.log("click drawCards");
+
+  // 1º capturar el numero de cartas a generar desde el input
+  let amountOfCards = document.querySelector("#amountOfCards").value;
+  console.log(amountOfCards);
+  generateRandomCards(amountOfCards);
+});
+
+// 3º metodo de gerar cartas aleatorias (función)
+function generateRandomCards(amountOfCards) {
+  let hand = document.querySelector("#hand");
+  hand.innerHTML = "";
+  // 4º gererar cartas aleatorias (bucle)
+  for (let i = 0; i < amountOfCards; i++) {
+    let cardElement = document.createElement("div");
+    cardElement.innerHTML = "CARTA" + i;
+    hand.appendChild(cardElement);
+  }
+}
